c4a0ab32683038265b1151973634fce663000fe7
aarooshgr <aaroosh@gmail.com>
2024-03-28 20:10:00 -0400

    Edited docstring

--- cfspopcon/algorithms/mcrf_from_fixed_P_SOL.py
+++ cfspopcon/algorithms/mcrf_from_fixed_P_SOL.py
@@ -10,14 +10,15 @@ def run_calc_mcrf_from_fixed_P_sol(P_in, P_sol_target):
     local_vars = locals()
     return {key: convert_to_default_units(local_vars[key], key) for key in RETURN_KEYS}
 
-"""Calculate the minimum core radiated fraction from a fixed value of P_SOL (namely P_sol_target)
+    """Calculate the minimum core radiated fraction from a fixed value of P_SOL (i.e. P_sol_target)
+
     Args:
-        minimum_core_radiated_fraction: :term:`glossary link<minimum_core_radiated_fraction>`
+        P_in: :term:`glossary link<P_in>`
         P_sol_target: :term:`glossary link<P_sol_target>`           # make a blurb?
 
     Returns:
-        :term:`core_radiator_concentration`, :term:`P_radiated_by_core_radiator`, :term:`P_radiation`, :term:`core_radiator_concentration`, :term:`core_radiator_charge_state`, :term:`zeff_change_from_core_rad` :term:`dilution_change_from_core_rad`, :term:`z_effective`, :term:`dilution`
-"""
+        minimum_core_radiated_fraction: :term:`glossary link<minimum_core_radiated_fraction>`
+    """
 
 calc_mcrf_from_fixed_P_sol = Algorithm(
     function=run_calc_mcrf_from_fixed_P_sol,
