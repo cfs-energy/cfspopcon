8254fc76fb0c106ba196768ad2ef8c57d44cd00d
aarooshgr <aaroosh@gmail.com>
2024-04-23 22:12:40 -0400

    got rid of unnecessary comment

--- cfspopcon/formulas/read_atomic_data.py
+++ cfspopcon/formulas/read_atomic_data.py
@@ -30,7 +30,7 @@ class AtomicData:
     NoncoronalLz = 3
     NoncoronalZ = 4
 
-    def __init__(self, atomic_data_directory: Path = Path() / "radas_dir") -> None: # previously "radas_dir"
+    def __init__(self, atomic_data_directory: Path = Path() / "radas_dir") -> None:
         """Initializes the AtomicData object by loading atomic data from the specified directory.
 
         Parameters:
