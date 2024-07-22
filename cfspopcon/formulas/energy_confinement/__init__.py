89fedcaaff873cf71ad5cf24f53cf7755d65fa99
AudreySaltzman <audreysa@mit.edu>
2024-07-22 19:17:50 -0400

    Add switch_to_L_mode_confinement_below_threshold to __init__.py

--- cfspopcon/formulas/energy_confinement/__init__.py
+++ cfspopcon/formulas/energy_confinement/__init__.py
@@ -3,7 +3,7 @@
 from .plasma_stored_energy import calc_plasma_stored_energy
 from .read_energy_confinement_scalings import ConfinementScaling, read_confinement_scalings
 from .solve_for_input_power import solve_tau_e_scaling_for_input_power
-from .switch_confinement_scaling_on_threshold import switch_to_linearised_ohmic_confinement_below_threshold
+from .switch_confinement_scaling_on_threshold import switch_to_linearised_ohmic_confinement_below_threshold, switch_to_L_mode_confinement_below_threshold
 
 __all__ = [
     "calc_plasma_stored_energy",
@@ -11,4 +11,5 @@ __all__ = [
     "ConfinementScaling",
     "solve_tau_e_scaling_for_input_power",
     "switch_to_linearised_ohmic_confinement_below_threshold",
+    "switch_to_L_mode_confinement_below_threshold",
 ]
