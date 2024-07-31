1450d739ca1297e0fdec2203800f257bcf71dd75
Christoph Hasse <hassec@users.noreply.github.com>
2024-07-31 11:19:16 -0400

    refactor(ipdb): move ipdb to dev dependencies

--- cfspopcon/cli.py
+++ cfspopcon/cli.py
@@ -2,26 +2,22 @@
 # Run this script from the repository directory.
 """CLI for cfspopcon."""
 
-import warnings
 from pathlib import Path
 
 import click
 import matplotlib.pyplot as plt
 import xarray as xr
-from ipdb import launch_ipdb_on_exception  # type:ignore[import-untyped]
 
 from cfspopcon import file_io
 from cfspopcon.input_file_handling import read_case
 from cfspopcon.plotting import make_plot, read_plot_style
-from cfspopcon.unit_handling import UnitStrippedWarning
 
 
 @click.command()
 @click.argument("case", type=click.Path(exists=True))
 @click.option("--dict", "-d", "kwargs", type=(str, str), multiple=True, help="Command-line arguments, takes precedence over config.")
 @click.option("--show", is_flag=True, help="Display an interactive figure of the result.")
-@click.option("--debug", is_flag=True, help="Enable the ipdb exception catcher.")
-def run_popcon_cli(case: str, show: bool, debug: bool, kwargs: tuple[tuple[str, str]]) -> None:
+def run_popcon_cli(case: str, show: bool, kwargs: tuple[tuple[str, str]]) -> None:
     """Run POPCON from the command line.
 
     This function uses "Click" to develop the command line interface. You can execute it using
@@ -29,13 +25,7 @@ def run_popcon_cli(case: str, show: bool, debug: bool, kwargs: tuple[tuple[str, 
     """
     cli_args: dict[str, str] = dict(kwargs)
 
-    if not debug:
-        run_popcon(case, show, cli_args)
-    else:
-        with launch_ipdb_on_exception():
-            with warnings.catch_warnings():
-                warnings.simplefilter("error", category=UnitStrippedWarning)
-                run_popcon(case, show, cli_args)
+    run_popcon(case, show, cli_args)
 
 
 @click.command()
