a7b9b7c7f007c01c6942073f271604a598872d38
Tom Body <116750897+tbody-cfs@users.noreply.github.com>
2024-08-05 13:31:01 -0400

    Apply coupling factor to RF power only (#87)

--- cfspopcon/formulas/auxillary_power/auxillary_power.py
+++ cfspopcon/formulas/auxillary_power/auxillary_power.py
@@ -4,7 +4,21 @@ from ...algorithm_class import Algorithm
 from ...unit_handling import Unitfull, ureg
 
 
-@Algorithm.register_algorithm(return_keys=["P_auxillary"])
-def calc_auxillary_power(P_external: Unitfull, P_ohmic: Unitfull) -> Unitfull:
-    """Calculates the required auxillary power."""
-    return (P_external - P_ohmic).clip(min=0.0 * ureg.MW)
+@Algorithm.register_algorithm(return_keys=["P_external", "P_auxillary_absorbed", "P_auxillary_launched"])
+def calc_auxillary_power(P_in: Unitfull, P_alpha: Unitfull, P_ohmic: Unitfull, fraction_of_external_power_coupled: Unitfull) -> Unitfull:
+    """Calculate the required auxillary power.
+
+    Args:
+        P_in: [MW] :term:`glossary link<P_in>`
+        P_alpha: [MW] :term:`glossary link<P_alpha>`
+        P_ohmic: [MW] :term:`glossary link<P_ohmic>`
+        fraction_of_external_power_coupled: [~]: :term:`glossary link<fraction_of_external_power_coupled>`
+
+    Returns:
+        :term:`P_external` [MW], :term:`P_auxillary_absorbed` [MW], :term:`P_auxillary_launched` [MW]
+    """
+    P_external = (P_in - P_alpha).clip(min=0.0 * ureg.MW)
+    P_auxillary_absorbed = (P_external - P_ohmic).clip(min=0.0 * ureg.MW)
+    P_auxillary_launched = P_auxillary_absorbed / fraction_of_external_power_coupled
+
+    return P_external, P_auxillary_absorbed, P_auxillary_launched
