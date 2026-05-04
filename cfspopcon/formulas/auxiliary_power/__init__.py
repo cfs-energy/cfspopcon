b699e242507f66a31f2b8dc873f2dfdee4b8f64d
Tom Body <tbody@cfs.energy>
2026-05-04 13:29:42 -0400

    Make calc_input_power_for_fixed_auxiliary_power importable

--- cfspopcon/formulas/auxiliary_power/__init__.py
+++ cfspopcon/formulas/auxiliary_power/__init__.py
@@ -1,5 +1,5 @@
 """Routines to calculate the auxiliary (non-Ohmic, non-fusion) power."""
 
-from .auxiliary_power import calc_auxiliary_power
+from .auxiliary_power import calc_auxiliary_power, calc_input_power_for_fixed_auxiliary_power
 
-__all__ = ["calc_auxiliary_power"]
+__all__ = ["calc_auxiliary_power", "calc_input_power_for_fixed_auxiliary_power"]
